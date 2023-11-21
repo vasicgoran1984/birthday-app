8f464c31d4492dd3c232579ae57c813e3fd628e3
dragor.dev <dragor.dev@gmail.com>
2023-11-21 14:15:47 +0100

    Finished 169-birthday-reminder-complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+  const [people, setPeople] = useState(data);
+  
   return (
     <main>
     <section className='container'>
-      <h3>0 Birthday today</h3>
-      <List />
-      <button onClick={() => console.log('click me')} className="btn">Clear All</button>
+      <h3>{people.length} Birthdays today</h3>
+      <List people={people} />
+      <button onClick={() => setPeople([])} className="btn">Clear All</button>
     </section>
   </main>
   );
