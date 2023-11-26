b583b65e1c349f6194f86585d5c62ab9beb6771b
dragor.dev <dragor.dev@gmail.com>
2023-11-26 09:56:08 +0100

    Finished slider

--- src/App.js
+++ src/App.js
@@ -1,37 +1,64 @@
-import React, { useState } from 'react'
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
-
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 function App() {
 
-  const allCategories = ['all',...new Set(items.map((item) => item.category))];
-  const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(allCategories);
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
 
-    
-
-    const filterItems = (category) => {
-      if (category === 'all') {
-        setMenuItems(items);
-        return;
-      }
-      const newItems = items.filter((item) => item.category === category)
-      setMenuItems(newItems);
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    console.log(index);
+    if(index < 0) {
+      setIndex(lastIndex);
     }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people])
 
-    return (
-      <main>
-        <section className='menu section'>
-        <div className="title">
-          <h2>our menu</h2>
-            <div className="underline"></div>
-          </div>
-          <Categories filterItems={filterItems} categories={categories}/>
-          <Menu items={menuItems} />
-        </section>
-      </main>
-    );
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider)
+  }, [index])
+
+  return <section className='section'>
+    <div className='title'>
+    <h2>
+      <span>/</span>reviews
+    </h2>
+    </div>
+    <div className="section-center">
+      {people.map((person, personIndex) => {
+        const { id, image, name, title, quote } = person;
+        let position = 'nextSlide';
+        if (index === personIndex) {
+          position = 'activeSlide'
+        }
+        if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+          position = 'lastSlide'
+        }
+
+        return <article className={position} key={id}>
+          <img src={image} alt={name} className='person-img' />
+          <h4>{name}</h4>
+          <p className='title'>{title}</p>
+          <p className="text">{quote}</p>
+          <FaQuoteRight className='icon' />
+        </article>
+      })}
+      <button className='prev' onClick={() => setIndex(index - 1)}>
+        <FiChevronLeft />
+      </button>
+      <button className='next' onClick={() => setIndex(index + 1)}>
+        <FiChevronRight />
+      </button>
+    </div>
+  </section>
+
 }
 
-export default App
+export default App;
--- src/data.js
+++ src/data.js
@@ -1,75 +1,36 @@
-const menu = [
+const people = [
   {
     id: 1,
-    title: 'buttermilk pancakes',
-    category: 'breakfast',
-    price: 15.99,
-    img: './images/item-1.jpeg',
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    image: 'https://www.course-api.com/images/people/person-1.jpeg',
+    name: 'maria ferguson',
+    title: 'office manager',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
   },
   {
     id: 2,
-    title: 'diner double',
-    category: 'lunch',
-    price: 13.99,
-    img: './images/item-2.jpeg',
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    image: 'https://www.course-api.com/images/people/person-4.jpeg',
+    name: 'john doe',
+    title: 'regular guy',
+    quote:
+      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
   },
   {
     id: 3,
-    title: 'godzilla milkshake',
-    category: 'shakes',
-    price: 6.99,
-    img: './images/item-3.jpeg',
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    image: 'https://www.course-api.com/images/people/person-3.jpeg',
+    name: 'peter smith',
+    title: 'product designer',
+    quote:
+      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
   },
   {
     id: 4,
-    title: 'country delight',
-    category: 'breakfast',
-    price: 20.99,
-    img: './images/item-4.jpeg',
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
-  },
-  {
-    id: 5,
-    title: 'egg attack',
-    category: 'lunch',
-    price: 22.99,
-    img: './images/item-5.jpeg',
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
-  },
-  {
-    id: 6,
-    title: 'oreo dream',
-    category: 'shakes',
-    price: 18.99,
-    img: './images/item-6.jpeg',
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
-  },
-  {
-    id: 7,
-    title: 'bacon overflow',
-    category: 'breakfast',
-    price: 8.99,
-    img: './images/item-7.jpeg',
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
-  },
-  {
-    id: 8,
-    title: 'american classic',
-    category: 'lunch',
-    price: 12.99,
-    img: './images/item-8.jpeg',
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
-  },
-  {
-    id: 9,
-    title: 'quarantine buddy',
-    category: 'shakes',
-    price: 16.99,
-    img: './images/item-9.jpeg',
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    image: 'https://www.course-api.com/images/people/person-2.jpeg',
+    name: 'susan andersen',
+    title: 'the boss',
+    quote:
+      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
   },
 ];
-export default menu;
+
+export default people;
