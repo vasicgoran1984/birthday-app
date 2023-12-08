8d7ad568205694f6e04df623f2a692160133a73c
dragor.dev <dragor.dev@gmail.com>
2023-12-08 10:58:41 +0100

    Finished 141-my-context-API-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Container from './components/Container'
+import Login from './components/Login'
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Container />
+    </>
   )
 }
 
