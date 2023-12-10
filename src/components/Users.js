211fcbfd8f4221acb4143338f238038719de44df
dragor.dev <dragor.dev@gmail.com>
2023-12-10 10:49:32 +0100

    Finished 146-my-context-API-and-Reducer-Setup

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,33 +1,66 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import { data } from '../data';
 import { clear } from '@testing-library/user-event/dist/clear';
 
+const defaultState = {
+    people:data,
+    isLoader: '',
+}
+
+const reducer = (state, action) => {
+    
+    if (action.type === 'CLEAR_LIST') {
+        return {...state, people: []}
+    }
+
+    if (action.type === 'RESET_LIST') {
+        return {...state, people: data}
+    }
+
+    if (action.type === 'REMOVE_ITEM') {
+        console.log(action);
+        let newPeople = state.people.filter((person) => person.id !== action.payload.id)
+        return {...state, people: newPeople}
+    }
+
+    return state;
+}
+
 const Users = () => {
 
-    const [people, setPeople] = useState(data);
+    //We use useReducer instead useState
+    const [state, dispatch] = useReducer(reducer, defaultState);
+
+    // We do not need this
+    // const [people, setPeople] = useState(data);
 
     const removeItem = (id) => {
-        let newPeople = people.filter((person) => person.id !== id)
-        setPeople(newPeople);
+        dispatch({type: 'REMOVE_ITEM', payload: {id}})
+        // let newPeople = people.filter((person) => person.id !== id)
+        // setPeople(newPeople);
     }
 
     const resetPeople = () => {
-        setPeople(data);
+        dispatch({type: 'RESET_LIST'})
+        // setPeople(data);
     }
 
     const clearAll = () => {
-        setPeople([]);
+        dispatch({type: 'CLEAR_LIST'})
+        // setPeople([]);
     }
 
-    if (people < 1) {
+    if (state.people < 1) {
         return (
             <button className='btn' onClick={resetPeople}>Reset</button>
         )
     }
 
+    console.log(state);
+
   return (
     <div>
-        {people.map((person) => {
+        {state.people.map((person) => {
             const { id, name } = person;
 
 
